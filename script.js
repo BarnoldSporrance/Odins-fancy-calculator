50e68e3f3addc6428c673b69327a313b23382453
Barry Lawrence <barry.am.lawrence@gmail.com>
2020-09-04 05:47:20 +0300

    issue with passing numerical input from input function

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+let one = document.getElementById("1_Butt");
+let two = document.getElementById("2_Butt");
+let three = document.getElementById("3_Butt");
+let four = document.getElementById("4_Butt");
+let five = document.getElementById("5_Butt");
+let six = document.getElementById("6_Butt");
+let seven = document.getElementById("7_Butt");
+let eight = document.getElementById("8_Butt");
+let nine = document.getElementById("9_Butt");
 
+let display = document.getElementById("display");
+let numberInputArray = [];
+let realDisplay ="";
+let inputNumericalValue=0;
 
 // Basic operator functions
 function add(num1, num2) {
   return num1 + num2;
 }
-
 function subtract(num1, num2) {
   return num1 - num2;
 }
-
 function multiply(num1, num2) {
     return num1 * num2;
 }
-
 function divide (num1, num2) {
     return num1 / num2;
 }
@@ -32,7 +42,45 @@ function operate(operand, num1, num2) {
   return result;
 } // end operate function
 
-console.log(operate('divide',4,5));
+
+// functions to get input from buttons
+function inputPush(e){
+  // get string values for each number pressed
+  let inputVar = e.target.id.slice(0,1);
+  // push these 1-9 string values into a string array
+  numberInputArray.push(inputVar);
+  //remove the commas to create a string representing the display value
+  realDisplay = numberInputArray.join("");
+  // convert display string to a number for subsequent operations
+  inputNumericalValue = parseInt(realDisplay);
+  // display the current value on calculator screen
+  display.innerText = realDisplay;
+
+  return inputNumericalValue;
+}
+
+
+// event listeners for the number buttons
+one.onclick = inputPush;
+two.onclick = inputPush;
+three.onclick = inputPush;
+four.onclick = inputPush;
+five.onclick = inputPush;
+six.onclick = inputPush;
+seven.onclick = inputPush;
+eight.onclick = inputPush;
+nine.onclick = inputPush;
+
+//console.log("inputNumericalValue after function:" + inputNumericalValue);
+
+
+operate('add',inputNumericalValue, 2);
+
+
+
 
   
+
+
+
  

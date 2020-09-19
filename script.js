c3c4d0d19e29f690b8641c23f2ce7c9a27191363
Barry Lawrence <barrylawrence@Barringtons-iMac.local>
2020-09-19 12:41:24 +0300

    fixed polarity bug

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// A ridiculously large list of way too many global variables. Ouch.
+// Too many global variables
 let zero = document.getElementById("0_Butt");
 let one = document.getElementById("1_Butt");
 let two = document.getElementById("2_Butt");
@@ -9,19 +9,15 @@ let six = document.getElementById("6_Butt");
 let seven = document.getElementById("7_Butt");
 let eight = document.getElementById("8_Butt");
 let nine = document.getElementById("9_Butt");
-
 let addOperand = document.getElementById("+_Butt");
 let posNegTogg = document.getElementById("+-ToggleButt");
 let subtractOperand = document.getElementById("-_Butt");
 let multiplyOperand = document.getElementById("*_Butt");
 let divideOperand = document.getElementById("/_Butt");
 let equalsOperand = document.getElementById("=_Butt");
-
 let percentOperand = document.getElementById("%_Butt");
-
 let allClear = document.getElementById("ac_Butt");
 let miniClear = document.getElementById("c_Butt");
-
 let display = document.getElementById("display");
 let miniDisplay = document.getElementById("miniDisplay");
 let numberInputArray = [];
@@ -36,7 +32,6 @@ let calcCounter = 0;
 let calcRecordArray = [];
 let noCommaCalcRecordArray=[];
 let calcResult;
-
 // Basic operator functions
 function add(num1, num2) {
   return num1 + num2;
@@ -50,15 +45,12 @@ function multiply(num1, num2) {
 function divide (num1, num2) {
     return num1 / num2;
 }
-
 //function to operate on two numbers given an operator
 function operate(operand, num1, num2) {
   let result = 0;
 if ((num1) && (num2 === 0) && (operand ==='/')) {
-  
   result = "ERROR";
   display.innerText = result;
-  
 } else if (operand === '+'){
      result = add(num1,num2);
   } else if (operand === '-') {
@@ -70,23 +62,15 @@ if ((num1) && (num2 === 0) && (operand ==='/')) {
         } 
   return result;
 } // end operate function
-
 // functions to get input from 1-9 number buttons
 function inputPush(e){
   soundPlay();
-  // get string values for each number pressed
   let inputVar = e.target.id.slice(0,1);
-  // push these 1-9 string values into a string array
-
-
   numberInputArray.push(inputVar);
-  //remove the commas to create a string representing the display value
   realDisplay = numberInputArray.join("");
   // convert display string to a number for subsequent operations
   inputNumericalValue = parseInt(realDisplay);
-
   calcRecordArray.push(inputNumericalValue);
-
   // display the current value on calculator screen
   display.innerText = inputNumericalValue;
   // decide which number to assign the current input to - first or second number in the current calculation
@@ -98,21 +82,17 @@ function inputPush(e){
   //  calcRecordArray.push(num2)
   }
   calcCounter++;
- 
   equalsOperand.onclick = function(){
     soundPlay();
     //calcRecordArray.pop();
     calcRecordArray.push(inputNumericalValue);
-    calcResult = operate(operandVar,num1,num2);
-       
-  // calculate the number of digits in the number
-  let numLength = String(calcResult).match(/\d/g).length;
- 
-  // round the number to required decimal places
-  if (numLength>3){
-    calcResult = parseFloat(calcResult.toFixed(2));
+    calcResult = operate(operandVar,num1,num2);  
+    // calculate the number of digits in the number
+    let numLength = String(calcResult).match(/\d/g).length;
+    // round the number to required decimal places
+    if (numLength>3){
+      calcResult = parseFloat(calcResult.toFixed(2));
     }
-
     // ensure that operations can be layered on top of the last calculation
     if ((operandCounter ==="post") && (calcCounter>0)){
       num1 = calcResult;
@@ -121,47 +101,33 @@ function inputPush(e){
     display.innerText = calcResult;
   }
 }
-
 // function to get the operator used in a calculation
 function operandPush(e){
   soundPlay();
   operandVar = e.target.id.slice(0,1);
-  
   //add the pre operand integer to the mini display when there is no calcuation result available
   let tempHolder = calcRecordArray[calcRecordArray.length-1];
-    calcRecordArray = [];
-    calcRecordArray.push(tempHolder);
-  
+  calcRecordArray = [];
+  calcRecordArray.push(tempHolder);
   calcRecordArray.push(operandVar);
-
   noCommaCalcRecordArray = calcRecordArray.join('');
-
   operandCounter = "post";
   numberInputArray=[];
   display.innerText = operandVar;
-
   miniDisplay.innerHTML = noCommaCalcRecordArray;
-  console.log("noCommaCalcRecordArray on operandPush: " + noCommaCalcRecordArray);
-
-
 //add result to minidisplay (if result available when operand pressed)
 if (calcResult) {
     calcRecordArray = [];
     calcRecordArray.push(calcResult);
-
     noCommaCalcRecordArray = calcRecordArray.join('');
     miniDisplay.innerHTML = noCommaCalcRecordArray;
-
+  }
 }
-
-}
-
 // function to clear everything, ready for a new calculation
 function clearAll(){
   soundPlay();
    numberInputArray = [];
    display.innerText = '';
-  
    inputNumericalValue = 0;
    operandCounter = "pre";
    num1 = 0;
@@ -174,41 +140,27 @@ function clearAll(){
    miniDisplay.innerText = '';
    noCommaCalcRecordArray =[];
 }
-
 // function to delete the last digit from thecurrent integer being entered
 function clearOne(){
   soundPlay();
   let popArray = Array.from(inputNumericalValue.toString());
-  
   popArray.pop();
   popArrayAfterJoin = popArray.join('');
   popArrayAfterParseInt = parseInt(popArrayAfterJoin);
- 
   display.innerText = '';
   inputNumericalValue = popArrayAfterParseInt;
- 
- 
-
   calcRecordArray = []
   calcRecordArray.push(inputNumericalValue);
   display.innerText = inputNumericalValue;
   if (inputNumericalValue === NaN) {
     display.innerText = "ERROR";
   }
-
-   
-
-
-   if (operandCounter === "pre") {
+  if (operandCounter === "pre") {
      num1 = inputNumericalValue;
-    
-    } else if(operandCounter === "post") {
-      num2 = inputNumericalValue;
-      }
-
-      
+     } else if(operandCounter === "post") {
+       num2 = inputNumericalValue;
+       }   
 }
-
 //function to toggle postive negative numbers
 function polarityToggle(){
   if (operandCounter === "pre") {
@@ -218,13 +170,20 @@ function polarityToggle(){
      num2 = -num2;
      display.innerText = num2;
      }
+  if (calcResult) {
+    calcResult = -calcResult;
+    display.innerText = calcResult;
+  }
+  if ((calcResult) && (num1)) {
+  num1 = calcResult;
+  }
 }
+function soundPlay(){
+  clickySound = document.getElementById("clickySound");
+  clickySound.currentTime = 0;
+  clickySound.play();
+  }
 
-
-
-
-
-// adding onclick events to buttons in super inefficent and clumsy fashion. Urgh. 
 zero.onclick = inputPush;
 one.onclick = inputPush;
 two.onclick = inputPush;
@@ -239,16 +198,8 @@ divideOperand.onclick = operandPush;
 addOperand.onclick = operandPush;
 subtractOperand.onclick = operandPush;
 multiplyOperand.onclick = operandPush;
-
 allClear.onclick = clearAll;
 miniClear.onclick = clearOne;
-
-function soundPlay(){
-clickySound = document.getElementById("clickySound");
-clickySound.currentTime = 0;
-clickySound.play();
-}
-
 posNegTogg.onclick = polarityToggle;
 
 
